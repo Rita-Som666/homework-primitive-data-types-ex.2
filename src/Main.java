f4701aac64f0b5daca48c78d4039df7ee870ff15
Tatiana Titova <ritasom666@gmail.com>
2023-10-29 16:11:00 +0400

    reformated

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ public class Main {
         int bill1 = 200;
         int bill2 = 3600;
         int bonus;
-        if(bill2 > 1000) {
-            bonus = bill2/100;
+        if (bill2 > 1000) {
+            bonus = bill2 / 100;
         } else {
             bonus = 0;
         }
-System.out.println("Ваша сумма пополнения " + bill2 + " Ваши бонусы " + bonus + " Ваш баланс " + (bill1+bill2+bonus));
+        System.out.println("Ваша сумма пополнения " + bill2 + " Ваши бонусы " + bonus + " Ваш баланс " + (bill1 + bill2 + bonus));
     }
 }
